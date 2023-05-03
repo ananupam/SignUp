464be63391af9ec49021e3cd5bef67548076c2d6
Anjali Anupam <2019024@iiitdmj.ac.in>
2023-05-03 12:44:15 +0530

    set routes for user

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("hello SignUp")
+const express = require('express');
+const userRouter = require('./routes/userRoutes');
+const app = express();
+
+app.use("/user",userRouter);
+
+app.get('/',(req,res)=>{
+    res.send('Hello, welcome to sign up')
+}) 
+
+
+app.listen(5004,()=>{
+    console.log("SignUp is running ")
+});
