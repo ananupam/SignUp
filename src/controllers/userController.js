43e72a4b103669dbdc5592f547bd3e11dca91f2a
Anjali Anupam <2019024@iiitdmj.ac.in>
2023-05-03 16:56:19 +0530

    create signin function

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,7 +8,6 @@ const signup = async(req,res) =>{
 
     //check existing user
     try{
-        
         const existingUser = await User.findOne({email : email});
         if(existingUser){
             return res.status(400).json({message:"User already exists!"});
@@ -24,17 +23,36 @@ const signup = async(req,res) =>{
             password: hashedPassowrd,
             email: email
         });
+
+        //generate token
         const token =jwt.sign({email: result.email, id:result._id},SECRET_KEY);
         res.status(201).json({user:result, token:token});
     }catch(error){
         console.log(error);
         res.status(500).json({message:"OOPS! Something went wrong."})
     }
-    res.send("Signup sucessfull");
 }
 
-const signin = (req,res) =>{
-    res.send("Signin sucessfull");
+const signin = async(req,res) =>{
+    const {email,password} = req.body;
+    try{
+        const existingUser = await User.findOne({email : email});
+
+        if(!existingUser){
+            return res.status(400).json({message:"User not found!"});
+        }
+
+        const matchPassword = await  bcrypt.compare(password,existingUser.password);
+        if(!matchPassword){
+            return res.status(400).json({message:"Wrong ID or password!"});
+        }
+
+        const token = jwt.sign({email: existingUser.email, id:existingUser._id},SECRET_KEY);
+        res.status(201).json({user:existingUser, token:token});
+    }catch(error){
+        console.log(error);
+        res.status(500).json({message:"OOPS! Something went wrong."})
+    }
 }
 
 module.exports = {signup,signin}
